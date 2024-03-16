02a57105c5b5452f64d268152b54bef17f5fe1ee
Pritam <pritambasu14@gmail.com>
2024-03-17 03:23:12 +0530

    add sequential animation for jarvis march

--- src/components/jarvis-march/JarvisMarch.tsx
+++ src/components/jarvis-march/JarvisMarch.tsx
@@ -3,14 +3,14 @@ import styles from './JarvisMarch.module.css'
 
 function JarvisMarch() {
     const { unityProvider } = useUnityContext({
-        loaderUrl: "JarvisMarch/Build/JarvisMarch.loader.js",
-        dataUrl: "JarvisMarch/Build/JarvisMarch.data",
-        frameworkUrl: "JarvisMarch/Build/JarvisMarch.framework.js",
-        codeUrl: "JarvisMarch/Build/JarvisMarch.wasm",
+        loaderUrl: "JarvisMarch/Build/builds.loader.js",
+        dataUrl: "JarvisMarch/Build/builds.data",
+        frameworkUrl: "JarvisMarch/Build/builds.framework.js",
+        codeUrl: "JarvisMarch/Build/builds.wasm",
     });
 
     return <div className={styles.container}>
-        <h1>This is the demo for the Jarvis March algorithm</h1>
+        <h1>Jarvis March Algorithm</h1>
         <Unity unityProvider={unityProvider} className={styles.canvas} />
     </div>;
 }
