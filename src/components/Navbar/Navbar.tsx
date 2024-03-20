037a46dab0ba1a4fa627bcd72e256e7805ed5ff8
Pritam <pritambasu14@gmail.com>
2024-03-20 21:49:25 +0530

    create a navbar component and add it to all routes

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import { Link } from "@tanstack/react-router";
+
+const Navbar = () => {
+    return (
+        <nav className="bg-blue-950 p-4 sticky top-0 z-50 opacity-100">
+            <div className="container mx-auto flex justify-between items-center">
+                <Link to="/" className="text-white hover:text-gray-300">Home</Link>
+                <div className="flex space-x-4">
+                    <Link to="/jarvis-march" className="text-white hover:text-gray-300">Jarvis March</Link>
+                    <Link to="/kirkpatrick-seidel" className="text-white hover:text-gray-300">Kirkpatrick-Seidel</Link>
+                    <Link to="/" className="text-white hover:text-gray-300">Implementation</Link>
+                    <Link to="/" className="text-white hover:text-gray-300">About</Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,10 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Fragment } from 'react/jsx-runtime'
+import Navbar from '../components/Navbar/Navbar'
 
 export const Route = createRootRoute({
-    component: () => <Outlet />,
+    component: () => <Fragment>
+        <Navbar />
+        <Outlet />
+    </Fragment>
 })
