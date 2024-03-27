3e6b59aa7b25c2f1fb75272236fcb3367a8fe665
Pritam <pritambasu14@gmail.com>
2024-03-28 02:32:33 +0530

    update navbar font to FiraCode

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,10 +2,10 @@ import { Link } from "@tanstack/react-router";
 
 const Navbar = () => {
     return (
-        <nav className="bg-blue-950 p-4 sticky top-0 z-50 opacity-100">
+        <nav className="bg-blue-950 p-4 sticky top-0 z-50 opacity-100 font-code">
             <div className="container mx-auto flex justify-between items-center">
                 <Link to="/" className="text-white hover:text-gray-300">Home</Link>
-                <div className="flex space-x-4">
+                <div className="flex space-x-6">
                     <Link to="/jarvis-march" className="text-white hover:text-gray-300">Jarvis March</Link>
                     <Link to="/kirkpatrick-seidel" className="text-white hover:text-gray-300">Kirkpatrick-Seidel</Link>
                     <Link to="/" className="text-white hover:text-gray-300">Implementation</Link>
