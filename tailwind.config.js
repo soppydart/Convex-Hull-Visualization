7a94e76319e57dbbcc55ccad0fd5fd096dc2aa8e
Pritam <pritambasu14@gmail.com>
2024-03-27 23:47:06 +0530

    add new font families to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      display: ['Roboto', 'sans-serif'],
+      body: ['Roboto', 'sans-serif'],
+      code: ['"FiraCode"', 'monospace']
+    },
   },
   plugins: [],
 }
