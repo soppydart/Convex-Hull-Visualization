e96c7f45e26096c46a5881db6b23a583b277549b
Pritam <pritambasu14@gmail.com>
2024-03-28 15:57:58 +0530

    change how random points are passed into the demo

--- src/components/KirkpatrickSiedel/KirkpatrickSeidelAlgorithm.tsx
+++ src/components/KirkpatrickSiedel/KirkpatrickSeidelAlgorithm.tsx
@@ -44,7 +44,7 @@ function KirkpatrickSeidel() {
             sendMessage("mainObject", "InitializePoints", point.x);
             sendMessage("mainObject", "InitializePoints", point.y);
         });
-        sendMessage("mainObject", "InitializePoints", -9999);
+        // sendMessage("mainObject", "InitializePoints", -9999);
     }
 
     function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
@@ -130,7 +130,7 @@ function KirkpatrickSeidel() {
                 <button onClick={handleClickRandom}
                     className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 
                 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 
-                dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 ml-5'>
+                dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 ml-5 mr-5'>
                     Plot random points</button>
                 <button onClick={handleReloadScene}
                     className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 
@@ -197,7 +197,7 @@ function KirkpatrickSeidel() {
             <h3 className="text-5xl font-medium text-white p-10 text-center mt-10">
                 Implementation
             </h3>
-            <div className='mx-auto max-w-4xl mt-5'>
+            {/* <div className='mx-auto max-w-4xl mt-5'>
                 <CopyBlock
                     language="cpp"
                     text={KirkpatrickSiedelImplementation}
@@ -206,7 +206,7 @@ function KirkpatrickSeidel() {
                     theme={dracula}
                     codeBlock={true}
                 />
-            </div>
+            </div> */}
         </div >
     );
 }
