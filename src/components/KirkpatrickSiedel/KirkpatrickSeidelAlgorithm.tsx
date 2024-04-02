915d9116da9717cc91f3fb182bf07b97b056f93e
Pritam <pritambasu14@gmail.com>
2024-04-02 19:18:36 +0530

    make sure the buttons are not being displayed if the demo is not loaded yet

--- src/components/KirkpatrickSiedel/KirkpatrickSeidelAlgorithm.tsx
+++ src/components/KirkpatrickSiedel/KirkpatrickSeidelAlgorithm.tsx
@@ -104,7 +104,7 @@ function KirkpatrickSeidel() {
                     borderRadius: '1%',
                 }} />
             </div>
-            <div className='flex justify-center mt-7'>
+            {isLoaded && <div className='flex justify-center mt-7'>
                 <div >
                     <input
                         id="file-upload"
@@ -136,7 +136,7 @@ function KirkpatrickSeidel() {
                 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 
                 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 ml-5'>
                     Reload Demo</button>
-            </div>
+            </div>}
 
             <h3 className="text-5xl font-medium text-white p-10 text-center mt-10">
                 Description
