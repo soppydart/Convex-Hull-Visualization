37d02d71d5a8c75ff8ce5ade6b0514e465ab8d32
Pritam <pritambasu14@gmail.com>
2024-04-04 07:33:26 +0530

    fix navbar: nav side bar should collapse on clicking anywhere else on the screen

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "@tanstack/react-router";
 
 const Navbar = () => {
-    const [isNavOpen, setIsNavOpen] = useState(false);
+    const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
+    const navRef = useRef<HTMLDivElement>(null);
+
     const toggleNav = () => {
         setIsNavOpen(!isNavOpen);
     };
 
+    useEffect(() => {
+        const handleOutsideClick = (event: MouseEvent) => {
+            if (navRef.current && !navRef.current.contains(event.target as Node)) {
+                setIsNavOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleOutsideClick);
+
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick);
+        };
+    }, []);
+
     return (
-        <nav className="bg-blue-950 p-4 sticky top-0 z-50 opacity-100 font-code">
+        <nav className="bg-blue-950 text-sm sm:text-base p-2 sm:p-4 sticky top-0 z-50 opacity-100 border-slate-800 font-code">
             <div className="container mx-auto flex justify-between items-center">
                 <Link onClick={() => {
                     if (isNavOpen) toggleNav();
@@ -29,36 +45,36 @@ const Navbar = () => {
             </div>
             {isNavOpen &&
 
-                <aside className="fixed top-0 right-0 z-40 w-50 h-70 transition-transform text-white
+                <aside ref={navRef} className="fixed top-0 right-0 z-40 w-40 h-70 transition-transform text-white
                 rounded border-2 border-slate-800 md:hidden">
 
-                    <div className="h-full px-3 py-4 overflow-y-auto bg-blue-950">
+                    <div className="h-full px-0 py-4 overflow-y-auto bg-blue-950 text-xs">
                         <button onClick={toggleNav} className="text-white hover:text-gray-300
-                        ml-40">
+                        float-right pr-1">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M4 6h16 M4 12h16 M4 18h16"></path>
                             </svg>
                         </button>
-                        <ul className="space-y-2 font-medium ms-3">
-                            <li className="flex justify-center">
+                        <ul className="space-y-2 font-medium ms-3 mt-10">
+                            <li className="flex justify-left">
                                 <Link onClick={toggleNav} to="/jarvis-march" className="p-2 rounded-lg
                                 text-white hover:text-gray-400 hover:underline">
                                     Jarvis March
                                 </Link>
                             </li>
-                            <li className="flex justify-center">
+                            <li className="flex justify-left">
                                 <Link onClick={toggleNav} to="/kirkpatrick-seidel" className="p-2 rounded-lg
                                 text-white hover:text-gray-400 hover:underline">
                                     Kirkpatrick-Seidel
                                 </Link>
                             </li>
-                            <li className="flex justify-center">
+                            <li className="flex justify-left">
                                 <Link onClick={toggleNav} to="/analysis" className="p-2 rounded-lg
                                 text-white hover:text-gray-400 hover:underline">
                                     Analysis
                                 </Link>
                             </li>
-                            <li className="flex justify-center">
+                            <li className="flex justify-left">
                                 <Link onClick={toggleNav} to="/about" className="p-2 rounded-lg
                                 text-white hover:text-gray-400 hover:underline">
                                     About
