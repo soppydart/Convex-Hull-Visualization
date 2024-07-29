d8e05dcd479a1597c05691166692fb0c49585343
Pritam <pritambasu14@gmail.com>
2024-07-30 01:20:59 +0530

    fix typo in the 'code' font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     fontFamily: {
-      display: ['Roboto', 'sans-serif'],
-      body: ['Roboto', 'sans-serif'],
-      code: ['"FiraCode"', 'monospace']
+      display: ["Roboto", "sans-serif"],
+      body: ["Roboto", "sans-serif"],
+      code: ["Fira Code", "monospace"],
     },
   },
   plugins: [],
-}
-
+};
